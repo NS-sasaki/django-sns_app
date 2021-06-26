29fea61b8284fd84813b91bdc57a2d907509eb22
NS-sasaki <81552518+NS-sasaki@users.noreply.github.com>
2021-06-26 22:03:03 +0900

    modified for heroku

--- sns/settings.py
+++ sns/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x5f@-1)=#ji@f35lq2zh%up@tku@ry8r!4%zh&9+fwce6ayduy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sns.urls'
